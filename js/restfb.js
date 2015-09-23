ccfe42555c8e7ea87359ca34305a472749775bb9
Norbert Bartels <n.bartels@phpmonkeys.de>
2015-09-23 13:19:06 +0200

    NoIssue: restfb user logos replaces with a responsive carousel implementation

--- js/restfb.js
+++ js/restfb.js
@@ -1153,6 +1153,38 @@
 		    .tooltip('show')
 	})
 
+  $('.restfb-users-carousel').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  })
+
     })
 
 }(jQuery)
