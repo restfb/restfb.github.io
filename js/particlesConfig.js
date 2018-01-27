57122c7e298fdb4ba3b3da16c7ef68045cd8da54
Norbert Bartels <n.bartels@phpmonkeys.de>
2018-01-27 23:52:29 +0100

    Issue #137 - visual effect added to 'landing' page

---
+++ js/particlesConfig.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	Particles.init({
+		selector: '.background',
+		color: '#337ab7',
+		connectParticles: true,
+		maxParticles: 150,
+		responsive: [{
+			breakpoint: 768,
+			options: {
+				maxParticles: 0
+			}
+		}]
+	});
+});
